a1fcc45f00bbf5d88abc4b32836335b10d384280
Slava <lozovoyv@gmail.com>
2019-01-21 05:37:03 +0300

    Update bootstrap and versions

--- core/Bootstrap/app.php
+++ core/Bootstrap/app.php
@@ -4,7 +4,25 @@ $baseDir = dirname(__DIR__, 2);
 
 $app = new \OpxCoreFramework\Application($baseDir);
 
-// Make bindings of implementations to interfaces here
+/*
+* Make bindings of implementations to interfaces
+*/
 
+// Bind config cache driver with parameters
+$app->bind(
+    \OpxCore\Interfaces\ConfigCacheRepositoryInterface::class,
+    \OpxCore\Config\ConfigCacheFile::class,
+    ['path' => $app->path('cache/config'), 'prefix' => 'config', 'extension' => 'cache']
+);
+
+// Bind config repository driver with parameters
+$app->bind(
+    \OpxCore\Interfaces\ConfigRepositoryInterface::class,
+    \OpxCore\Config\ConfigFiles::class,
+    ['path' => $app->path('config')]
+);
+
+// Bind config driver
+$app->bind(\OpxCore\Interfaces\ConfigInterface::class, \OpxCore\Config\Config::class);
 
 return $app;
