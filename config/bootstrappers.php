1644e4e6d2225de1f14fa066c9009d3ce4f66960
lozovoyv <lozovoyv@gmail.com>
2021-02-17 21:28:21 +0300

    add bootstrappers config file

---
+++ config/bootstrappers.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    // List of bootstrappers in order of bootstrapping.
+    // Required dependencies could be assigned here or bound to container externally,
+    // for example in application bootstrap file.
+    //
+    // Example:
+    // Bootstrapper::class,
+    // AnotherBootstrapper::class => ['driver' => Driver::class]
+];
