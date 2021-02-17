f66c2581675e46ca798b1d03d9ca2a914f527ac2
lozovoyv <lozovoyv@gmail.com>
2021-02-17 21:51:21 +0300

    Update description

--- config/bootstrappers.php
+++ config/bootstrappers.php
@@ -3,7 +3,9 @@
 return [
     // List of bootstrappers in order of bootstrapping.
     // Required dependencies could be assigned here or bound to container externally,
-    // for example in application bootstrap file.
+    // for example in application bootstrap file (../app.php).
+    //
+    // Each bootstrapper must implement OpxCore\App\Interfaces\AppBootstrapperInterface
     //
     // Example:
     // Bootstrapper::class,
